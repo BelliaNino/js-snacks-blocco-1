9ce96558adc9076f7e46f7c7ae38d40dc9953fa6
BelliaNino <bellianino1996@gmail.com>
2026-02-05 10:36:41 +0100

    end exercise n° 1

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,3 +4,15 @@ console.log(`test`);
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
+const numbOne = parseInt(prompt(`Type any number`))
+const numbTwo = parseInt(prompt(`Type onother number`))
+
+if (numbOne > numbTwo) {
+    console.log(numbOne);
+    
+}
+
+else if (numbOne < numbTwo) {
+    console.log(numbTwo);
+    
+}
